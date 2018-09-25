9d0876fe9eda4a09295a8d092d19dddfe12ba085
Pavithra R <pavianil84@gmail.com>
2018-09-25 15:52:02 +0530

    Initial commit

---
+++ src/qsp/DemoA.java
@@ -0,0 +1,16 @@
+package qsp;
+
+import java.io.File;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class DemoA {
+public static void main(String[]args) throws Exception{
+	Workbook w = WorkbookFactory.create(new File("./data/book1.xlsx"));
+
+	String s=w.getSheet("Sheet1").getRow(0).getCell(0).toString();
+System.out.println(s);	
+}
+}
+
+
